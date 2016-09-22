fd4071d1060966aa3d78bfef068f7a44d3475540
Ooglermarc <tumelo.manale@gmail.com>
2016-09-22 07:59:56 +0200

    mailgun addon

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-   address:              'smtp.zoho.com',
-   port:                 465,
-   domain:               'invitted.com',
-   user_name:            'tumi@invitted.com',
-   password:             'xxP@ssword12',
-   authentication:       'plain',
-   enable_starttls_auto: true 
-}
