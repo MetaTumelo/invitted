2d7b509893291f21922a038ce79228692a5b8acd
Ooglermarc <tumelo.manale@gmail.com>
2016-09-22 07:38:27 +0200

    Time selector

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'tumi@invitted.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,19 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+   address:              'smtp.zoho.com',
+   port:                 465,
+   domain:               'invitted.com',
+   user_name:            'tumi@invitted.com',
+   password:             'xxP@ssword12',
+   authentication:       'plain',
+   enable_starttls_auto: true  
+}
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
