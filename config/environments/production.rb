286b050eb7093a2fcea796c6cdfb1e09b31ad9d2
Ooglermarc <tumelo.manale@gmail.com>
2016-09-27 11:06:34 +0200

    AWS and Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.assets.debug = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
+ 
 
   config.paperclip_defaults = {
   storage: :s3,
